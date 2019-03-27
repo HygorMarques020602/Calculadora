da81c938de69909bdfa6dc205f60560ccf1af27c
Hygor <srjota123@outlook.com>
2019-03-27 14:44:47 -0300

    Adicionando botão multiplicação a calculadora

--- Calculadora/src/br/edu/ifcvideira/Calculadora/Calculadora.java
+++ Calculadora/src/br/edu/ifcvideira/Calculadora/Calculadora.java
@@ -107,6 +107,19 @@ public class Calculadora extends JFrame {
 		contentPane.add(btnSubtrao);
 		
 		JButton btnMultiplicao = new JButton("X");
+		btnMultiplicao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String vl1 = txVl1.getText();
+				String vl2 = txVl2.getText();
+				
+				double vl1d = Double.parseDouble(vl1);
+				double vl2d = Double.parseDouble(vl2);
+				
+				double total = vl1d * vl2d;
+				
+				JOptionPane.showMessageDialog(null, "O resultado da subtração é: "+total);
+			}
+		});
 		btnMultiplicao.setBounds(218, 159, 89, 23);
 		contentPane.add(btnMultiplicao);
 		
